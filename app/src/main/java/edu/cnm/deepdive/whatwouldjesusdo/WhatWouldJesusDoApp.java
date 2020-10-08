9823f0d079530427634fba699f8f820978e0c64a
jkelly97 <jmkelly2041@gmail.com>
2020-10-08 17:29:57 -0500

    Dependencies updated

---
+++ app/src/main/java/edu/cnm/deepdive/whatwouldjesusdo/WhatWouldJesusDoApp.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.whatwouldjesusdo;
+
+import android.app.Application;
+import com.facebook.stetho.Stetho;
+
+public class WhatWouldJesusDoApp extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    Stetho.initializeWithDefaults(this);
+  }
+}
