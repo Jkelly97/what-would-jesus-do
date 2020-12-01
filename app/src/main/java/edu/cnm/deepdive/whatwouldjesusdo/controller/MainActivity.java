b532289e27b04d579eb8395bf9f3c18f0ac19fbc
jkelly97 <jmkelly2041@gmail.com>
2020-12-01 10:23:56 -0600

    Failing attempt at a dark theme

--- app/src/main/java/edu/cnm/deepdive/whatwouldjesusdo/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/whatwouldjesusdo/controller/MainActivity.java
@@ -1,9 +1,12 @@
 package edu.cnm.deepdive.whatwouldjesusdo.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.ui.AppBarConfiguration.Builder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -48,6 +51,18 @@ public class MainActivity extends AppCompatActivity {
     NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
     NavigationUI.setupWithNavController(navigationView, navController);
   }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setTheme(R.style.DarkTheme);
+    setContentView(R.layout.activity_main);
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setTheme(R.style.LightTheme);
+    setContentView(R.layout.activity_main);
+  }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,4 +77,21 @@ public class MainActivity extends AppCompatActivity {
     return NavigationUI.navigateUp(navController, mAppBarConfiguration)
         || super.onSupportNavigateUp();
   }
+
+  TextView tvChangeTheme = findViewById(R.id.tvChangeTheme);
+tvChangeTheme.setOnClickListener(new View.OnClickListener() {
+
+    @Override
+    public void onClick(View v) {
+
+      if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+      } else {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+      }
+
+      finish();
+      startActivity(new Intent(MainActivity.this, MainActivity.this.getClass()));
+    }
+  });
 }
